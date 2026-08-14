fda446314d50ed14f3896e14d2f77420eb4e2bb4
MuX-retro <usmanovmuhsin3@gmail.com>
2026-08-14 21:56:47 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN не задан в переменных окружения!")
-
-AI_PRICE = 300  # цена AI-подписки в звёздах
-AI_DURATION = 30 * 24 * 3600  # 30 дней
